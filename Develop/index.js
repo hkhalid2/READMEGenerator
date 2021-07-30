705604372ce9ed86a432362c62fddb5a4cd6f8a2
Hamza Khalid <hamzer95@gmail.com>
2021-07-30 16:41:34 -0400

    created prompt for questions in order to generate readME

--- Develop/index.js
+++ Develop/index.js
@@ -3,8 +3,59 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions  = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the Project's title?",
+      },
+
+      {
+        type: "input",
+        name: "description",
+        message: "What is the Project's description?",
+      },
+
+      {
+        type: "input",
+        name: "installation",
+        message: "What are the Project's installation instructions?",
+      },
+
+      {
+        type: "input",
+        name: "usage",
+        message: "What is the Project's usage information?",
+      },
+
+      {
+        type: "input",
+        name: "contribution",
+        message: "What are the Project's contribution guidelines?",
+      },
+
+      {
+        type: "input",
+        name: "test",
+        message: "What are the Project's test instructions?",
+      },
+
+      {
+        type: "input",
+        name: "github",
+        message: "What is the your Github profile username ?",
+      },
+
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+    
+    ]);
+  };;
 
 
 // uses promises to create writeFile function
@@ -15,7 +66,7 @@ const generateREADME = (answers) =>
 
 // TODO: Create a function to initialize app
 const init = () => {
-    promptUser()
+    questions()
       .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
       .then(() => console.log('Successfully wrote to README.md'))
       .catch((err) => console.error(err));
