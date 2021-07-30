51b18fd15f10263e479fe24e011199b44b591dbe
Hamza Khalid <hamzer95@gmail.com>
2021-07-30 16:14:22 -0400

    added packages needed for app as well as init app/promies to create writeFile

--- Develop/index.js
+++ Develop/index.js
@@ -6,11 +6,20 @@ const util = require('util');
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// uses promises to create writeFile function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const generateREADME = (answers) =>
+  ``;
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptUser()
+      .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+      .then(() => console.log('Successfully wrote to README.md'))
+      .catch((err) => console.error(err));
+  };
 
 // Function call to initialize app
 init();
