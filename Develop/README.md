f62f3be398cc9791c9e53770b75cc898372cbb7f
Hamza Khalid <hamzer95@gmail.com>
2021-08-01 20:09:35 -0400

    got license symbols and placeholder notices to be added to readme

---
+++ Develop/README.md
@@ -0,0 +1,36 @@
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+# Read Me Generator
+
+## Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Usage Information](#Usage-Information)
+* [License](#License)
+* [Contribution Guidelines](#Contribution-Guidelines)
+* [Questions](#Questions)
+
+## Description
+blah blah
+
+## Installation
+```
+npm install
+```
+
+## Usage Information
+```
+node index
+```
+
+## License
+```
+Apache License 2.0
+blah
+```
+
+## Contribution Guidelines
+add contributions
+
+## Questions
+Github: https://github.com/hkhalid2  
+For additional questions email : hkhalid2@gmu.edu
--- Develop/index.js
+++ Develop/index.js
@@ -33,7 +33,7 @@ const questions  = () => {
       {
         type: 'list',
         name: 'license',
-        message: 'Pick a license',
+        message: 'Which license do you need?',
         choices: [
          'MIT License',
          'Apache License 2.0',
@@ -68,15 +68,41 @@ const questions  = () => {
         message: "What is your email address?",
       },
     
-    ]);
-  };;
+    ])
+
+  };
+
+const licenseprops = (answers) => {
+        if (answers.license === 'MIT License') {
+            answers.symbol = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            answers.info = "blah";
+        }
+        else if (answers.license === 'Apache License 2.0') {
+            answers.symbol = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            answers.info = "blah";
+        }
+        else if (answers.license === 'The Unlicense') {
+            answers.symbol = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+            answers.info = "blah";
+        }
+        else if (answers.license === 'Mozilla Public License 2.0') {
+            answers.symbol = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+            answers.info = "blah";
+        }
+        else {
+            answers.license.symbol = "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+            answers.license.info = "blah";
+        }
+        return answers;
+};
 
 
 // uses promises to create writeFile function
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const generateREADME = (answers) =>
-`# ${answers.title}
+`${answers.symbol}
+# ${answers.title}
 
 ## Table of Contents
 * [Description](#Description)
@@ -102,6 +128,7 @@ ${answers.usage}
 ## License
 \`\`\`
 ${answers.license}
+${answers.info}
 \`\`\`
 
 ## Contribution Guidelines
@@ -114,7 +141,8 @@ For additional questions email : ${answers.email}`;
 // TODO: Create a function to initialize app
 const init = () => {
     questions()
-      .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+      .then((answers) => licenseprops(answers))
+      .then((response) => writeFileAsync('README.md', generateREADME(response)))
       .then(() => console.log('Successfully wrote to README.md'))
       .catch((err) => console.error(err));
   };
